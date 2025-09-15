3bcfc24e01d3a640e1e4aadd27403c4e35f91ea1
ahmed <ahmedsukkary52##@gmail.com>
2025-09-15 16:36:44 +0300

    + Add colors

---
+++ lib/Utils/color_app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class ColorApp {
+  static const Color whiteFF = Color(0xFFFFFFFF);
+  static const Color blue56 = Color(0xFF5669FF);
+  static const Color whiteF4 = Color(0xFFF4EBDC);
+  static const Color black0F = Color(0xFF0F0F0F);
+  static const Color primaryWhiteF2 = Color(0xFFF2FEFF);
+  static const Color primaryDarkBlue10 = Color(0xFF101127);
+  static const Color whiteF0 = Color(0xFFF0F0F0);
+  static const Color black1C = Color(0xFF1C1C1C);
+  static const Color red = Color(0xFFD80027);
+  static const Color gray = Color(0xFF7B7B7B);
+  static const Color transparent = Colors.transparent;
+}
---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+void main (){
+  runApp(EventApp());
+}
+class EventApp extends StatelessWidget {
+  const EventApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
